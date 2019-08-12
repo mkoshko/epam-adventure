33a4044c8b807300cb8c1286ccefef15b1f03207
Maxim Koshko <amol3d.by@gmail.com>
2019-08-12 23:56:22 +0300

    package-info tests

---
+++ task02/src/test/java/by/koshko/task02/service/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Tests for service classes.
+ *
+ * @author Maxim Koshko
+ * @version 1.0
+ * @see by.koshko.task02.service.MatrixMultiplicationTest
+ * @see by.koshko.task02.service.MatrixGeneratorImplTest
+ * @see by.koshko.task02.service.MatrixDataParserTest
+ * @see by.koshko.task02.service.MatrixCreatorServiceImplTest
+ */
+package by.koshko.task02.service;
