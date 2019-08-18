3355187f2c8f5cb6d5ebd2761027208dacd2810c
Maxim Koshko <amol3d.by@gmail.com>
2019-08-18 16:00:11 +0300

    MonkeyService

--- task03/src/main/java/by/koshko/task03/main/Main.java
+++ task03/src/main/java/by/koshko/task03/main/Main.java
@@ -1,18 +1,22 @@
 package by.koshko.task03.main;
 
 
+import by.koshko.task03.entity.Component;
+import by.koshko.task03.entity.ComponentType;
 import by.koshko.task03.entity.TextComposite;
 import by.koshko.task03.service.*;
 
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.StringJoiner;
 
 public class Main {
     public static void main(final String[] args) {
         StringJoiner joiner = new StringJoiner("\n\t");
-        joiner.add("The organization of a simple bus-oriented computer is shown in Fig. 2-1. The\nCPU(Central Processing Unit) is the ‘‘brain’’ of the computer. Its function is to\nexecute programs stored in the main memory by fetching their instructions, exam-\nining them, and then executing them one after another.\n The components are con-\nnected by a bus, which is a collection of parallel wires for transmitting address,\ndata, and control signals. Buses can be external to the CPU, connecting it to mem-\nory and I/O devices, but also internal to the CPU, as we will see shortly. Modern\n computers have multiple buses.");
-        joiner.add("The CPU is composed of several distinct parts. The control unit is responsible for fetching instructions from main memory and determining their type. The arithmetic logic unit performs operations such as addition and Boolean AND needed to carry out the instructions.");
-        joiner.add("The CPU also contains a small, high-speed memory used to store temporary results and certain control information. This memory is made up of a number of registers, each having has a certain size and function. Usually, all the registers have the same size. Each register can hold one number, up to some maximum determined by its size. Registers can be read and written at high speed since they are internal to the CPU.");
-        joiner.add("The most important register is theProgram Counter(PC), which points to the next instruction to be fetched for execution. (The name ‘‘program counter’’ is somewhat misleading because it has nothing to do with counting anything, but the term is universally used.) Also important is theInstruction Register(IR), which holds the instruction currently being executed. Most computers have numerous other registers as well, some of them general purpose as well as some for specific purposes. Yet other registers are used by the operating system to control the computer.");
+        joiner.add("6The organization of a simple bus-oriented computer is shown in Fig. 2-1. The\nCPU(Central Processing Unit) is the ‘‘brain’’ of the computer. Its function is to\nexecute programs stored in the main memory by fetching their instructions, exam-\nining them, and then executing them one after another.\n The components are con-\nnected by a bus, which is a collection of parallel wires for transmitting address,\ndata, and control signals. Buses can be external to the CPU, connecting it to mem-\nory and I/O devices, but also internal to the CPU, as we will see shortly. Modern\n computers have multiple buses.");
+        joiner.add("3The CPU is composed of several distinct parts. The control unit is responsible\n for fetching instructions from main memory and determining their type. The arithmetic\n logic unit performs operations such as addition and Boolean AND needed to\n carry out the instructions.");
+        joiner.add("5The CPU also contains a small, high-speed memory used to store temporary results\n and certain control information. This memory is made up of a number of registers,\n each having has a certain size and function. Usually, all the registers have\n the same size. Each register can hold one number, up to some maximum determined\n by its size. Registers can be read and written at high\n speed since they are internal to the CPU.");
+        joiner.add("4The most important register is theProgram Counter(PC), which points to the next\n instruction to be fetched for execution. (The name ‘‘program counter’’ is somewhat \nmisleading because it has nothing to do with counting anything, but the term is \nuniversally used.) Also important is theInstruction Register(IR), which holds\n the instruction currently being executed. Most computers have numerous other registers\n as well, some of them general purpose as well as some for specific purposes. Yet\n other registers are used by the operating system to control the computer.");
         //===============================//
         TextParser textParser = new TextParser();
         ParagraphParser paragraphParser = new ParagraphParser();
@@ -26,6 +30,12 @@ public class Main {
         //===============================//
         TextComposite textComposite = new TextComposite();
         textParser.parse(joiner.toString(), textComposite);
-        System.out.println(textComposite.getChild(0).getChild(1).compose());
+//        System.out.println(textComposite.compose());
+        //===============================//
+        MonkeyService service = new MonkeyService();
+        var list = service.obtain(textComposite, ComponentType.PARAGRAPH);
+        System.out.println(list.size());
+        list.sort(Comparator.comparingInt(Component::size));
+        list.forEach(component -> System.out.println(component.compose()));
     }
 }
---
+++ task03/src/main/java/by/koshko/task03/service/MonkeyService.java
@@ -0,0 +1,32 @@
+package by.koshko.task03.service;
+
+import by.koshko.task03.entity.Component;
+import by.koshko.task03.entity.ComponentType;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class MonkeyService {
+    private MonkeyService() {
+    }
+
+    private static void find(final Component component,
+                             final ComponentType type,
+                             final List<Component> components) {
+        if (component.getType() == type) {
+            components.add(component);
+        } else {
+            if (component.size() > 0) {
+                for (int i = 0; i < component.size(); i++) {
+                    find(component.getChild(i), type, components);
+                }
+            }
+        }
+    }
+
+    public static List<Component> obtain(final Component component, final ComponentType type) {
+        var components = new ArrayList<Component>();
+        find(component, type, components);
+        return components;
+    }
+}
