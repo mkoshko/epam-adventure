1518b496261eaffabef83e56765e3e1f22e7198f
Maxim Koshko <amol3d.by@gmail.com>
2019-08-25 22:40:42 +0300

    SortingService tests

---
+++ task03/src/test/java/by/koshko/task03/service/SortingServiceTest.java
@@ -0,0 +1,58 @@
+package by.koshko.task03.service;
+
+import by.koshko.task03.dao.DaoException;
+import by.koshko.task03.dao.TextReader;
+import by.koshko.task03.entity.TextComposite;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+/**
+ * Tests for {@link SortingService} methods.
+ */
+public class SortingServiceTest {
+
+    /**
+     * Original text.
+     */
+    private String textToSort;
+    /**
+     * Sorted text.
+     */
+    private String expectedText;
+    /**
+     * Path to original text.
+     */
+    private final String path = getClass()
+            .getResource("/data/sortSentenceNumber.txt").getPath();
+    /**
+     * Path to sorted text.
+     */
+    private final String pathToSorted = getClass()
+            .getResource("/data/sortedSentenceNumber.txt").getPath();
+
+    /**
+     * Read text from file.
+     *
+     * @throws DaoException see {@link TextReader} docs.
+     */
+    @BeforeTest
+    public void setUp() throws DaoException {
+        textToSort = TextReader.read(path);
+        expectedText = TextReader.read(pathToSorted);
+    }
+
+    /**
+     * Sort paragraphs in text by sentence number and compare with expected.
+     *
+     * @throws ServiceException see {@link SortingService} docs.
+     */
+    @Test
+    public void testSortBySentenceNumber() throws ServiceException {
+        TextComposite text = new TextComposite();
+        ParserHolder.getTextParser().parse(textToSort, text);
+        SortingService.sortBySentencesNumber(text);
+        assertEquals(text.compose(), expectedText);
+    }
+}
