02079ecbffd7bbd4ef2ccf48f451afaeeaa3ad81
Maxim <amol3d.by@gmail.com>
2019-09-13 12:19:48 +0300

    create table script

--- cyberwikia/sql/3_create_tables.sql
+++ cyberwikia/sql/3_create_tables.sql
@@ -11,12 +11,6 @@ CREATE TABLE country
     CONSTRAINT UQ_country UNIQUE (id, name, code)
 );
 
-CREATE TABLE role
-(
-    id TINYINT(1) UNSIGNED,
-    CONSTRAINT PK_role PRIMARY KEY (id)
-);
-
 CREATE TABLE user
 (
     id       INT UNSIGNED        NOT NULL AUTO_INCREMENT,
@@ -31,29 +25,26 @@ CREATE TABLE user
 
 CREATE TABLE player
 (
-    id              INT UNSIGNED     NOT NULL AUTO_INCREMENT,
-    nickname        VARCHAR(50)      NOT NULL,
-    firstName       VARCHAR(30)      NOT NULL,
-    lastName        VARCHAR(30)      NOT NULL,
-    birth           DATE,
-    nationality     TINYINT UNSIGNED NOT NULL,
-    team            INT UNSIGNED,
-    approx_winnings INT,
+    id         INT UNSIGNED     NOT NULL AUTO_INCREMENT,
+    nickname   VARCHAR(50)      NOT NULL,
+    firstName  VARCHAR(30)      NOT NULL,
+    lastName   VARCHAR(30)      NOT NULL,
+    birth      DATE,
+    country_id TINYINT UNSIGNED NOT NULL,
     CONSTRAINT PK_player PRIMARY KEY (id)
 );
 
 CREATE TABLE team
 (
-    id              INT UNSIGNED     NOT NULL AUTO_INCREMENT,
-    name            VARCHAR(50)      NOT NULL,
-    logo_file       VARCHAR(4096),
-    location        TINYINT UNSIGNED NOT NULL,
-    creator         INT UNSIGNED,
-    captain         INT UNSIGNED,
-    coach           INT UNSIGNED,
-    approx_winnings INT,
-    game            INT UNSIGNED     NOT NULL,
-    overview        MEDIUMTEXT,
+    id         INT UNSIGNED     NOT NULL AUTO_INCREMENT,
+    name       VARCHAR(50)      NOT NULL,
+    logo_file  VARCHAR(4096),
+    country_id TINYINT UNSIGNED NOT NULL,
+    creator    INT UNSIGNED,
+    captain    INT UNSIGNED,
+    coach      INT UNSIGNED,
+    game       INT UNSIGNED     NOT NULL,
+    overview   MEDIUMTEXT,
     CONSTRAINT PK_team PRIMARY KEY (id),
     CONSTRAINT UQ_team UNIQUE (id, name)
 );
@@ -67,6 +58,7 @@ CREATE TABLE game
     CONSTRAINT UQ_games UNIQUE (title)
 );
 
+# Optional feature. Can be removed.
 CREATE TABLE m2m_player_team_invite
 (
     id        INT UNSIGNED NOT NULL,
@@ -102,32 +94,23 @@ CREATE TABLE m2m_tournament_team
     CONSTRAINT PK_tournament_team PRIMARY KEY (tournament_id, team_id)
 );
 
-CREATE TABLE m2m_user_team
+CREATE TABLE m2m_player_team
 (
     user_id    INT UNSIGNED NOT NULL,
     team_id    INT UNSIGNED NOT NULL,
     active     BOOL         NOT NULL,
     join_date  DATE         NOT NULL,
     leave_date DATE         NOT NULL,
-    CONSTRAINT PK_user_team PRIMARY KEY (user_id, team_id)
+    CONSTRAINT PK_player_team PRIMARY KEY (user_id, team_id)
 );
-
-CREATE TABLE m2m_user_player
-(
-    user_id   INT UNSIGNED NOT NULL,
-    player_id INT UNSIGNED NOT NULL
-);
-
-ALTER TABLE user
-    ADD CONSTRAINT FK_user_role FOREIGN KEY (role) REFERENCES role (id);
 
 ALTER TABLE player
-    ADD CONSTRAINT FK_player_nationality FOREIGN KEY (nationality) REFERENCES country (id),
-    ADD CONSTRAINT FK_player_team FOREIGN KEY (team) REFERENCES team (id);
+    ADD CONSTRAINT FK_player_id FOREIGN KEY (id) REFERENCES user(id),
+    ADD CONSTRAINT FK_player_country_id FOREIGN KEY (country_id) REFERENCES country (id);
 
 ALTER TABLE team
     ADD CONSTRAINT FK_team_creator FOREIGN KEY (creator) REFERENCES player (id),
-    ADD CONSTRAINT FK_team_location FOREIGN KEY (location) REFERENCES country (id),
+    ADD CONSTRAINT FK_team_location FOREIGN KEY (country_id) REFERENCES country (id),
     ADD CONSTRAINT FK_team_captain FOREIGN KEY (coach) REFERENCES player (id),
     ADD CONSTRAINT FK_team_game FOREIGN KEY (game) REFERENCES game (id);
 
@@ -138,13 +121,9 @@ ALTER TABLE m2m_tournament_team
     ADD CONSTRAINT FK_m2m_tournament_team_tournament_id FOREIGN KEY (tournament_id) REFERENCES tournament (id),
     ADD CONSTRAINT FK_m2m_tournament_team_team_id FOREIGN KEY (team_id) REFERENCES team (id);
 
-ALTER TABLE m2m_user_team
+ALTER TABLE m2m_player_team
     ADD CONSTRAINT FK_user_team_user_id FOREIGN KEY (user_id) REFERENCES user (id),
     ADD CONSTRAINT FK_user_team_team_id FOREIGN KEY (team_id) REFERENCES team (id);
-
-ALTER TABLE m2m_user_player
-    ADD CONSTRAINT FK_m2m_user_player_user_id FOREIGN KEY (user_id) REFERENCES user (id),
-    ADD CONSTRAINT FK_m2m_user_player_player_id FOREIGN KEY (player_id) REFERENCES player (id);
 
 ALTER TABLE m2m_player_team_invite
     ADD CONSTRAINT FK_m2m_player_team_invite_player_id FOREIGN KEY (player_id) REFERENCES player (id),
