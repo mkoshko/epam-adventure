9e346fcef61b380d5e3b0857348d2a822c0505f7
Maxim Koshko <amol3d.by@gmail.com>
2019-10-08 15:44:18 +0300

    tournament service add CRUD operations

--- cyberwikia/src/main/java/by/koshko/cyberwikia/service/TournamentService.java
+++ cyberwikia/src/main/java/by/koshko/cyberwikia/service/TournamentService.java
@@ -5,4 +5,6 @@ import by.koshko.cyberwikia.bean.Tournament;
 public interface TournamentService {
     Tournament getTournamentById(long id) throws ServiceException;
     void createTournament(Tournament tournament) throws ServiceException;
+    void updateTournament(Tournament tournament) throws ServiceException;
+    void deleteTournament(Tournament tournament) throws ServiceException;
 }
